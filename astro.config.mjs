5d6c6104ecfaf39a501c475f6131b54367160d30
Venu005 <venusaiyalamanchili@gmail.com>
2024-06-03 19:45:10 +0530

    chore: bug fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,49 @@ import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 
+
 export default defineConfig({
-  output: "server",
-  adapter: vercel(),
-  integrations: [react(), mdx(), tailwind(), sitemap()],
+  site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
+  base: config.site.base_path ? config.site.base_path : "/",
+  trailingSlash: config.site.trailing_slash ? "always" : "never",
+  image: {
+    service: squooshImageService(),
+  },
+  integrations: [
+    react(),
+    sitemap(),
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    AutoImport({
+      imports: [
+        "./src/layouts/components/ui/Button",
+        "./src/layouts/components/ui/Accordion",
+        "./src/layouts/components/ui/Notice",
+        "./src/layouts/components/ui/Video",
+        "./src/layouts/components/ui/Youtube",
+        "./src/layouts/components/ui/Tabs",
+        "./src/layouts/components/ui/Tab",
+      ],
+    }),
+    mdx(),
+  ],
+  markdown: {
+    remarkPlugins: [
+      remarkToc,
+      [
+        remarkCollapse,
+        {
+          test: "Table of contents",
+        },
+      ],
+    ],
+    shikiConfig: {
+      theme: "one-dark-pro",
+      wrap: true,
+    },
+    extendDefaultPlugins: true,
+  },
 });
