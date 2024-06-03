cf8c15491357fffef57bf8987f87fb3cdd00260b
Venu005 <venusaiyalamanchili@gmail.com>
2024-06-03 20:03:02 +0530

    chore:site fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwind from "@astrojs/tailwind";
 
 
 export default defineConfig({
-  site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
+  //site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
   image: {
